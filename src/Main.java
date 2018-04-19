2448da0dd4397c45a17ecf6dce95adf9e8e4baff
astonkus19 <astonkus19@holynames-sea.org>
2018-04-19 07:52:03 -0700

    a tad bit confused, but i am working through that

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
     /**
      * Puts the elements in the arrayList in sorted order from smallest to greatest.
      * This function uses selection sort to sort the arrayList.
+     *
      * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
      */
     public static void selectionSort(ArrayList<Integer> arrayList) {
@@ -20,8 +21,9 @@ public class Main {
     /**
      * Returns the index that value is located in the arrayList. This function searches linearly in
      * the arrayList to find the value.
+     *
      * @param arrayList the ArrayList containing the list of values to search. arrayList cannot contain duplicates
-     * @param value the value we are looking for in the array list
+     * @param value     the value we are looking for in the array list
      */
     public static int linearSearch(ArrayList<Integer> arrayList, int value) {
         throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
@@ -30,9 +32,10 @@ public class Main {
     /**
      * Returns the index that value is located in the arrayList. This function uses binary search in
      * the arrayList to find the value.
+     *
      * @param arrayList the ArrayList containing the list of values to search. THIS ARRAYLIST MUST BE
      *                  IN SORTED ORDER. arrayList cannot contain duplicates
-     * @param value the value we are looking for in the array list
+     * @param value     the value we are looking for in the array list
      */
     public static int binarySearch(ArrayList<Integer> arrayList, int value) {
         throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
@@ -41,9 +44,12 @@ public class Main {
     /**
      * Puts the elements in the arrayList in sorted order from smallest to greatest.
      * This function uses MergeSort to sort the arrayList.
+     *
      * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
      */
     public static void mergeSort(ArrayList<Integer> arrayList) {
+
+
         throw new UnsupportedOperationException("mergeSort() has not been implemented yet");
     }
 
@@ -52,9 +58,10 @@ public class Main {
      * The function sorts the portion of arrayList specified by the range [lo, hi). The range
      * includes lo but excludes hi (arrayList[lo] is the first element in the range, but
      * arrayList[hi] is the first element after the last element in the range).
+     *
      * @param arrayList the ArrayList to be sorted.
-     * @param lo the index of the first element in the range
-     * @param hi the index of the last element in the range + 1.
+     * @param lo        the index of the first element in the range
+     * @param hi        the index of the last element in the range + 1.
      */
     public static void sort(ArrayList<Integer> arrayList, int lo, int hi) {
         throw new UnsupportedOperationException("sort() has not been implemented yet");
@@ -65,12 +72,22 @@ public class Main {
      * The function merges two consecutive, sorted ranges in the arrayList into one sorted range. The ranges
      * are specified as [lo, mid) and [mid, hi). Each range includes the first element, but excludes
      * the last element (the same way as in sort()).
+     *
      * @param arrayList the ArrayList to be sorted.
-     * @param lo the index of the first element in the first range
-     * @param mid the boundary point of the two ranges. arrayList[mid] is in the second range.
-     * @param hi the index of the last element in the second range + 1.
+     * @param lo        the index of the first element in the first range
+     * @param mid       the boundary point of the two ranges. arrayList[mid] is in the second range.
+     * @param hi        the index of the last element in the second range + 1.
      */
     public static void merge(ArrayList<Integer> arrayList, int lo, int mid, int hi) {
-        throw new UnsupportedOperationException("merge() has not been implemented yet");
+        ArrayList<Integer> nums = new ArrayList<Integer>;
+        int[] numsArray = new int[hi - lo];
+        int k = 0;
+        for (int i = lo; i < hi; i++)
+            if (arrayList.get(lo + k) <= mid + k)
+                nums.add(arrayList.get(lo + k));
+                k++;
+
+
+
     }
 }
